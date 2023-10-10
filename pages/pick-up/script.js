75946eff7b8eade5de2655fa896e04e28cc49796
qzflank <fynn.labs@vier.ai>
2023-10-10 15:06:37 +0200

    made viewport for tablet and for desktop

--- pages/pick-up/script.js
+++ pages/pick-up/script.js
@@ -9,8 +9,9 @@ const shoppingCart = document.getElementById('shoppingCart')
 const shoppingCartPop = document.getElementById('shoppingCartPop')
 const shoppingCartPopContent = document.getElementById('shoppingCartPopContent')
 const shoppingCartPopOrderBtn = document.getElementById('shoppingCartPopOrderBtn')
-const totalPrice = document.createElement('div')
-
+const totalWrapper = document.createElement('div')
+const totalText = document.createElement('span')
+const totalPrice = document.createElement('span')
 let shoppingNumber = 0
 let shoppingCartItems = []
 let count = true;
@@ -105,6 +106,7 @@ function renderFilteredProducts(filteredProducts) {
         const productImage = document.createElement('img');
         const pricePlusWrapper = document.createElement('div');
         const plus = document.createElement('div');
+        const textWrapper = document.createElement('div')
         productHeadline.textContent = product.name;
         productPrice.textContent = `$${product.price}`;
         plus.textContent = `+`;
@@ -113,8 +115,9 @@ function renderFilteredProducts(filteredProducts) {
         productImage.alt = product.alt;
         productDescription.textContent = product.description;
         productListDiv.appendChild(productWrapper);
-        productWrapper.appendChild(productHeadline);
-        productWrapper.appendChild(productDescription);
+        productWrapper.appendChild(textWrapper)
+        textWrapper.appendChild(productHeadline);
+        textWrapper.appendChild(productDescription);
         productWrapper.appendChild(productImage);
         productWrapper.appendChild(pricePlusWrapper);
         pricePlusWrapper.appendChild(productPrice);
@@ -123,10 +126,11 @@ function renderFilteredProducts(filteredProducts) {
         pricePlusWrapper.classList.add("pricePlusWrapper")
         productPrice.classList.add("price");
         productHeadline.classList.add("text__headline");
-        productImage.classList.add("left__image");
+        productImage.classList.add("image");
         productDescription.classList.add("text__description");
         productWrapper.classList.add("imagetext__wrapper");
         productHeadDesWrapper.classList.add("headDesWrapper");
+        textWrapper.classList.add("textWrapper")
 
         plus.addEventListener('click', function () {
             plusBtnClick(product);
@@ -170,19 +174,27 @@ function plusBtnClick(product) {
         shoppingCartItemWrapper.appendChild(shoppingCartItemName);
         shoppingCartItemWrapper.appendChild(shoppingCartItemPrice);
     });
-    shoppingCartPopContent.appendChild(totalPrice);
+
+    shoppingCartPopContent.appendChild(totalWrapper);
+    totalWrapper.appendChild(totalText)
+    totalWrapper.appendChild(totalPrice)
+    totalWrapper.classList.add('total__wrapper')
+    totalText.classList.add('total__text')
     shoppingCartPopOrderBtn.style.opacity = 1;
 }
+
+const backgroundPop = document.getElementById('backgroundPop')
 
 //toggles the shoppingCart
 function showShoppingCart() {
     updateTotalPrice();
     shoppingCartPop.classList.toggle('shoppingCartShow');
+    backgroundPop.classList.toggle('backgroundBlendShow')
     if (shoppingNumber === 0 && count === true) {
-        const shopppingCartPlaceholder = document.createElement('div')
-        shopppingCartPlaceholder.textContent = 'Noch keine Einträge im Warenkorb'
-        shopppingCartPlaceholder.classList.add('shopppingCart__placeholder')
-        shoppingCartPopContent.appendChild(shopppingCartPlaceholder)
+        const shoppingCartPlaceholder = document.createElement('div')
+        shoppingCartPlaceholder.textContent = 'Noch keine Einträge im Warenkorb'
+        shoppingCartPlaceholder.classList.add('shoppingCart__placeholder')
+        shoppingCartPopContent.appendChild(shoppingCartPlaceholder)
         count = false;
     }
 
@@ -198,5 +210,6 @@ function calculateTotalPrice() {
 
 function updateTotalPrice() {
     const totalAmount = calculateTotalPrice();
-    totalPrice.textContent = `Summe: $${totalAmount.toFixed(2)}`;
+    totalPrice.textContent = `$${totalAmount.toFixed(2)}`;
+    totalText.textContent = `Summe:`
 }
